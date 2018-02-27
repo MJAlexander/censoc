f771aa72471406bd1bd24146bf38583c680a9458
MJAlexander <monicaalexander@berkeley.edu>
2018-02-26 19:07:12 -0800

    function to load census data

---
+++ R/helpers.R
@@ -0,0 +1,31 @@
+#' Get first word from a string
+#'
+#' @param x a string with spaces between words
+#' @return first word from string of words
+#' @examples
+#' get_first_word("Agatha Christie")
+
+
+get_first_word <- function(x)
+{
+  x.split <- strsplit(x, split = " ")
+  x1.list <- lapply(x.split,`[`, 1) # returns NA if no name
+  x1 <- unlist(x1.list)
+  return(x1)
+}
+
+#' Clean matching keys
+#'
+#' @param key a string that acts as a key that may have punctuation
+#' @return a string with no punctuation
+#' @examples
+#' clean_key("testi'ing")
+
+
+
+## function to clean keys
+clean_key <- function(key){
+  return(gsub(" +|[[:punct:]]", "", key))
+}
+
+
--- R/load_census.R
+++ R/load_census.R
@@ -1,11 +1,38 @@
-#' Load a census state file
+#' Load a census file
 #'
-#' @param state_file string indicating location of file
-#' @param cols_to_keep vector indicating colname names of variables to keep
-#'
+#' @param census_file string indicating location of file
+#' @param cols_to_keep vector indicating colname names of variables to keep. Default is just what is needed for match: first name, last name, age, sex, serial number, person number.
+#' @return a census dataframe with match key and unique IPUMS identifier
 
 load_census <- function(state_file,
-                              cols_to_keep){
-  fread(state_file)
-  return("Yay")
+                        cols_to_keep = NULL){
+
+  all_cols_to_keep <- c("SERIAL40", "PERNUM", "AGE", "SEX", "NAMELAST", "NAMEFRST")
+  if(!is.null(cols_to_keep)){
+    all_cols_to_keep <- c(all_cols_to_keep, cols_to_keep)
+  }
+
+  # read in census file, only keeping desired columns
+  census <- fread(census_file, select = all_cols_to_keep)
+
+  # clean variables
+  census[,"fname" := str_to_upper(NAMEFRST)]
+  census[,"fname" := get_first_word(fname)]
+  census[,"lname" := str_to_upper(NAMELAST)]
+  census[,"census_age" := as.numeric(AGE)]
+  # create unique ID
+  census[,"ipums_id" := paste(SERIAL40, PERNUM, sep = "_")]
+
+  # remove those with no name info
+  # remove blanks or anything that has question marks
+  census <- census[!(grepl("\\?", census$fname)|grepl("\\?", census$lname)|census$lname==""),]
+
+  # create key
+  census[,"tmp_key" := paste0(lname, fname, census_age)]
+  census[,"clean_key" := clean_key(tmp_key),]
+  census[,"n_clean_key" := .N, by = clean_key]
+  census <- census[,!"tmp_key"]
+
+  return(census)
+
 }
