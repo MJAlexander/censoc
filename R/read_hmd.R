91b47166006c31cd1d4cea3c2cbb32419e46381d
MJAlexander <monicaalexander@berkeley.edu>
2018-03-29 08:33:42 -0600

    added hmd read in function

---
+++ R/read_hmd.R
@@ -0,0 +1,22 @@
+#' Read in HMD text file
+#'
+#' @param file_name string indicating location of hmd text file
+#' @param add_country logicial indicating whether or not to add a column with the country name. Default is `FALSE`
+#' @param country_name country name
+#' @return a data table with the contents of HMD file read in.
+
+
+read_hmd <- function(file_name,
+                     add_country = FALSE,
+                     country_name = NULL){
+  d <- fread(file_name)
+  if(add_country){
+    if(is.null(country_name)){
+      stop("Need to supply country name")
+    }
+    else{
+      d$Country <- country_name
+    }
+  }
+  return(d)
+}
