4417ac2fcaefe929d641923697a59f7fdf63ac28
MJAlexander <monicaalexander@berkeley.edu>
2018-04-06 10:28:43 -0700

    cannot do basic math

--- R/add_age_of_death_column.R
+++ R/add_age_of_death_column.R
@@ -2,13 +2,13 @@
 #' Add age of death based on month and year of birth and death.
 #'
 #'@param dt a CenSoc data table
-#'@returns a CenSoc data table with `age_at_death` column.
+#'@return a CenSoc data table with `age_of_death` column.
 
 add_age_of_death_column <- function(dt){
   dt <- as.data.table(dt)
   if(sum(c("byear", "dyear", "bmonth", "dmonth") %in% colnames(dt))!=4){
     stop("Need to have the following columns to calculate age of death: byear, dyear, bmonth, dmonth.")
   }
-  dt[, age_of_death := ifelse(bmonth<dmonth, dyear-byear, dyear-byear-1)]
+  dt[, age_of_death := ifelse(bmonth<=dmonth, dyear-byear, dyear-byear-1)]
   return(dt)
 }
