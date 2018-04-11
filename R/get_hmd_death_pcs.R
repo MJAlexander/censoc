3d9606d59d2f8ef6716b6a6bac570e063fed39a3
MJAlexander <monicaalexander@berkeley.edu>
2018-04-11 08:11:09 -0700

    added option to demean before SVD

--- R/get_hmd_death_pcs.R
+++ R/get_hmd_death_pcs.R
@@ -4,11 +4,13 @@
 #'
 #' @param dx_data HMD data on death distribution
 #' @param single_country whether or not dataset contains single or multiple countries
+#' @param demeaned whether the values should be demeaned. If \code{TRUE}, the first column of the returned matrix is the mean.
 #' @param n_pcs number of principal components to return. Default is 10.
 #' @return a matrix that contains principal components by age of logit death distributions.
 
 
 get_hmd_death_pcs <- function(dx_data,
+                              demeaned = FALSE,
                               single_country,
                               n_pcs = 10){
   if(single_country){
@@ -17,7 +19,14 @@ get_hmd_death_pcs <- function(dx_data,
     px <- px[!is.na(apply(px, 1, sum)),]
     px[px==0]<- 0.0001
     logit_px <- logit(px)
-    svd_px <- svd(logit_px)
+    if(demeaned){
+      ax <- apply(logit_px, 2, mean)
+      dm_logit_px <- sweep(logit_px, 2, ax)
+      svd_px <- svd(dm_logit_px)
+    }
+    else{
+      svd_px <- svd(logit_px)
+    }
   }
   else{
     px <- dx_data %>%  select(age, country, year, prop) %>% spread(age, prop)
@@ -25,10 +34,20 @@ get_hmd_death_pcs <- function(dx_data,
     px <- px[!is.na(apply(px, 1, sum)),]
     px[px==0]<- 0.0001
     logit_px <- logit(px)
-    svd_px <- svd(logit_px)
+    if(demeaned){
+      ax <- apply(logit_px, 2, mean)
+      dm_logit_px <- sweep(logit_px, 2, ax)
+      svd_px <- svd(dm_logit_px)
+    }
+    else{
+      svd_px <- svd(logit_px)
+    }
   }
 
   pcs <- c()
+  if(demeaned){
+    pcs <- ax
+  }
   for(i in 1:n_pcs){
     pcs <- cbind(pcs, svd_px$v[, i])
   }
